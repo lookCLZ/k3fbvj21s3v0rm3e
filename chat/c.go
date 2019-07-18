fa0040ba48113d3325fcc2314a0a91e0fd3e5ee5
lookNa2CO3 <lookinward.qq@gmail.com>
2019-07-19 07:34:02 +0800

    3

--- chat/c.go
+++ chat/c.go
@@ -1,16 +1,16 @@
 package main
 
 import (
-	"net"
 	"fmt"
+	"net"
 	// "strings"
-	// "time"
+	"time"
 )
 
 // 客户端信息
 type Client struct {
-	C chan string
-	Name string 
+	C    chan string
+	Name string
 	Addr string
 }
 
@@ -21,12 +21,12 @@ var onlineMap = make(map[string]Client)
 var message = make(chan string)
 
 func main() {
-	listener,_:=net.Listen("tcp","127.0.0.1:8005")
+	listener, _ := net.Listen("tcp", "127.0.0.1:8005")
 	defer listener.Close()
 
 	go Manager()
 	for {
-		conn,_:=listener.Accept()
+		conn, _ := listener.Accept()
 		go HandlerConnect(conn)
 	}
 }
@@ -34,62 +34,84 @@ func main() {
 func HandlerConnect(conn net.Conn) {
 	defer conn.Close()
 
-	netAddr:=conn.RemoteAddr().String()
+	// 判断客户端是否断开
+	isQuit:=make(chan bool)
+	run:=make(chan bool)
 
-	clnt:=Client{
-		C:make(chan string),
-		Name:netAddr,
-		Addr:netAddr,
+	netAddr := conn.RemoteAddr().String()
+
+	clnt := Client{
+		C:    make(chan string),
+		Name: netAddr,
+		Addr: netAddr,
 	}
-	onlineMap[netAddr]=clnt
+	onlineMap[netAddr] = clnt
 
-	go WriteMsgToClient(conn,clnt)
-	message <- MakeMsg(clnt,"已上线\n")
+	go WriteMsgToClient(conn, clnt)
+	message <- MakeMsg(clnt, "已上线")
 	fmt.Println(2)
-	go ReadMsg(conn,clnt)
+	go ReadMsg(conn, clnt,isQuit,run)
 
-	for {}
+	for {
+		select {
+		case <-isQuit:
+			delete(onlineMap,clnt.Addr)
+			return
+		case <-run:
+		case <-time.After(time.Second * 60):
+			delete(onlineMap,clnt.Addr)
+			return
+		}
+	}
 }
 
 // 全go程管理器
 func Manager() {
 	for {
-		msg:=<-message
-		for _,v:=range onlineMap {
-			v.C<-msg
+		msg := <-message
+		for _, v := range onlineMap {
+			v.C <- msg
 		}
 	}
 }
 
 // 监听自己的耳朵，有就写入
-func WriteMsgToClient(conn net.Conn,clnt Client) {
-	for msg:=range clnt.C {
-		conn.Write([]byte(msg))
+func WriteMsgToClient(conn net.Conn, clnt Client) {
+	for msg := range clnt.C {
+		conn.Write([]byte(msg + "\n"))
 	}
 }
 
 // 组装用户的消息的格式
-func MakeMsg(clnt Client,msg string) (string) {
-	return clnt.Name +"："+ msg
+func MakeMsg(clnt Client, msg string) string {
+	return clnt.Name + "：" + msg
 }
 
 // 读取客户端发来的消息
-func ReadMsg(conn net.Conn,clnt Client) {
-	buf:=make([]byte,4096)
+func ReadMsg(conn net.Conn, clnt Client,isQuit,run chan<- bool) {
+	buf := make([]byte, 4096)
 	for {
-		n,_:=conn.Read(buf)
-		if n==0{ 
-			message <- MakeMsg(clnt, "刚刚下线了\n")
+		n, _ := conn.Read(buf)
+		if n == 0 {
+			isQuit<-true
+			message <- MakeMsg(clnt, "刚刚下线了")
 			return
-		} else if string(buf[:n])=="who" {
-			for _,v:=range onlineMap {
-				message<-v.Name
-			}
 		}
+		
+		msg := string(buf[:n-1])
+		run<-true
+		if msg == "who" {
+			fmt.Println(9)
+			for _, v := range onlineMap {
+				message <- "地址：" + v.Name
+			}
 
-		msg:=string(buf[:n])
-		message <- MakeMsg(clnt, msg)
-
+		} else if len(msg) > 8 && msg[:6] == "rename" {
+			clnt.Name = msg[7:]
+			onlineMap[clnt.Addr] = clnt
+			conn.Write([]byte("改名成功：" + clnt.Name + "\n"))
+		} else {
+			message <- MakeMsg(clnt, msg)
+		}
 	}
 }
-
