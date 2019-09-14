de73b8b87b7b480cc524d42ee640fef02147a8e2
lookNa2CO3 <lookinward.qq@gmail.com>
2019-09-14 10:45:09 +0800

    commit

--- crypto/对称加密/main.go
+++ crypto/对称加密/main.go
@@ -5,7 +5,7 @@ import "fmt"
 func main() {
 	fmt.Println("des 加密解密 cbc分组模式")
 	// 秘钥
-	key:=[]byte("1234abdd")
+	key:=[]byte("12345678")
 	// 源文本
 	src:=[]byte("你好，我是一段文本")
 	cipherText:=desEncrypt(src,key)
