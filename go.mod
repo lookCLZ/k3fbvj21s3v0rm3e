6f5ca050401594b6e1141bcabb8e72c925032f14
lookNa2CO3 <lookinward.qq@gmail.com>
2019-11-05 17:05:02 +0800

    x

--- go.mod
+++ go.mod
@@ -3,8 +3,14 @@ module github.com/lookCLZ/contacts
 go 1.12
 
 require (
+	github.com/EDDYCJY/gsema v0.0.0-20190120044130-7f6a61b75219 // indirect
 	github.com/andlabs/ui v0.0.0-20180902183112-867a9e5a498d
+	github.com/astaxie/beego v1.12.0
+	github.com/betterjun/pkg v0.0.0-20190724144456-f230171707b2
+	github.com/boltdb/bolt v1.3.1
 	github.com/golang/protobuf v1.3.2
+	github.com/shiena/ansicolor v0.0.0-20151119151921-a422bbe96644 // indirect
+	github.com/sirupsen/logrus v1.4.2
 	golang.org/x/net v0.0.0-20190916140828-c8589233b77d
 	google.golang.org/grpc v1.23.1
 )
--- temp/main.go
+++ temp/main.go
@@ -3,25 +3,12 @@ package main
 import (
 	"fmt"
 	"time"
-
-	"github.com/EDDYCJY/gsema"
+	// "github.com/EDDYCJY/gsema"
 )
 
-var sema = gsema.NewSemaphore(3)
-
 func main() {
-	userCount := 10
-	for i := 0; i < userCount; i++ {
-		go Read(i)
-	}
-
-	sema.Wait()
-}
-
-func Read(i int) {
-	defer sema.Done()
-	sema.Add(1)
-
-	fmt.Printf("go func: %d, time: %d\n", i, time.Now().Unix())
-	time.Sleep(time.Second)
+	// timeStr := time.Now().Format("2006-01-02 15:04:56")
+	t, _ := time.ParseInLocation("2006-01-02", "1994-03-01", time.Local)
+	timeUnix := t.Unix()
+	fmt.Println(timeUnix)
 }
