989c57492b63d8820ff0e839ba3eacc48a589422
lookNa2CO3 <lookinward.qq@gmail.com>
2019-11-20 18:57:14 +0800

    x

--- python3/webapp/www/app.py
+++ python3/webapp/www/app.py
@@ -21,7 +21,7 @@ def init(loop):
     app = web.Application(loop=loop)
     app.router.add_route('GET', '/', index)
     srv = yield from loop.create_server(app.make_handler(), '127.0.0.1', 9000)
-    logging.info('服务已启动： http://127.0.0.1:9000')
+    logging.info('服务已启动：http://127.0.0.1:9000')
     return srv
 
 loop = asyncio.get_event_loop()
