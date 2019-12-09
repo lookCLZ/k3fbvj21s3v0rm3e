b6c9ae9570f34acf9e9d9950c8f7caab71d3a7e5
lookNa2CO3 <lookinward.qq@gmail.com>
2019-12-09 09:39:40 +0800

    x

---
+++ rust/guessing_game/target/debug/deps/guessing_game-a9e5e42542bf8e3b.d
@@ -0,0 +1,5 @@
+/Users/liuhongrui/heima/contacts/rust/guessing_game/target/debug/deps/guessing_game-a9e5e42542bf8e3b: src/main.rs
+
+/Users/liuhongrui/heima/contacts/rust/guessing_game/target/debug/deps/guessing_game-a9e5e42542bf8e3b.d: src/main.rs
+
+src/main.rs:
---
+++ rust/guessing_game/target/debug/guessing_game.d
@@ -0,0 +1,1 @@
+/Users/liuhongrui/heima/contacts/rust/guessing_game/target/debug/guessing_game: /Users/liuhongrui/heima/contacts/rust/guessing_game/src/main.rs
